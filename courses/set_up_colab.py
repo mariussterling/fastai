f54393af52fd45c3a18d6d70f1608b0a795c1025
mariussterling <marius.sterling@hu-berlin.de>
2018-10-22 14:56:25 +0200

    add set up for colab

---
+++ courses/set_up_colab.py
@@ -0,0 +1,15 @@
+!pip install Pillow==4.1.1
+!pip install "fastai==0.7.0"
+!pip install torchtext==0.2.3
+!apt-get -qq install -y libsm6 libxext6 && pip install -q -U opencv-python
+import cv2
+from os import path
+from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag
+platform = '{}{}-{}'.format(get_abbr_impl(), get_impl_ver(), get_abi_tag())
+!apt update && apt install -y libsm6 libxext6
+accelerator = 'cu80' if path.exists('/opt/bin/nvidia-smi') else 'cpu'
+!pip install -q http://download.pytorch.org/whl/{accelerator}/torch-0.3.0.post4-{platform}-linux_x86_64.whl torchvision
+import torch
+!pip install image
+%matplotlib inline
+from fastai.imports import *
